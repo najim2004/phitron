60a2bb5548a74e7533e7832dcdbc2b39cb8f2b08
najim2004 <najim10112004@gmail.com>
2025-09-21 21:03:04 +0600

    Add Library and Fees tables with proper constraints and validation

--- database_management/module_4(assingment_1)/Question1.sql
+++ database_management/module_4(assingment_1)/Question1.sql
@@ -1,3 +1,10 @@
+-- Create tables
+-- Make a student table
+-- Make a Library table
+-- Make a Fees table
+-- Create table with proper relations.
+
+
 USE student;
 
 CREATE TABLE Student (
@@ -5,4 +12,18 @@ CREATE TABLE Student (
     roll INT,
     name VARCHAR(40),
     department VARCHAR(30)
+);
+
+CREATE TABLE Library (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    book_name VARCHAR(40),
+    author VARCHAR(40),
+    publisher VARCHAR(40)
+);
+CREATE TABLE Fees (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    student_id INT,
+    book_id INT,
+    fee_amount DECIMAL(10, 2),
+    due_date DATE
 );
--- database_management/module_4(assingment_1)/Question2.sql
+++ database_management/module_4(assingment_1)/Question2.sql
@@ -1,3 +1,6 @@
+-- Add proper constraints with the No 1 question
+
+
 USE student;
 
 CREATE TABLE Student (
@@ -5,4 +8,18 @@ CREATE TABLE Student (
     roll INT NOT NULL CHECK (roll>0),
     name VARCHAR(40) NOT NULL CHECK(LENGTH(name)>=3),
     department VARCHAR(30) NOT NULL CHECK(LENGTH(department)>=3)
+);
+
+CREATE TABLE Library (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    book_name VARCHAR(40) NOT NULL CHECK(LENGTH(book_name)>=3),
+    author VARCHAR(40) NOT NULL CHECK(LENGTH(author)>=3),
+    publisher VARCHAR(40) NOT NULL CHECK(LENGTH(publisher)>=3)
+);
+CREATE TABLE Fees (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    student_id INT NOT NULL CHECK(student_id>0),
+    book_id INT NOT NULL CHECK(book_id>0),
+    fee_amount DECIMAL(10, 2) NOT NULL CHECK(fee_amount>0),
+    due_date DATE NOT NULL CHECK(due_date>='2023-01-01')
 );
