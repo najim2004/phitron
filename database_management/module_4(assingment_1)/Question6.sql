3efad1dce3466cb353742948cf080f13126ee6d0
najim2004 <najim10112004@gmail.com>
2025-09-21 21:23:33 +0600

    Add SQL query to filter employees by age and department

---
+++ database_management/module_4(assingment_1)/Question6.sql
@@ -0,0 +1,3 @@
+-- Write a query to show the employee LastName whose age is greater than 30 and works in Marketing department.
+
+SELECT LastName FROM employee WHERE Age > 30 AND department = 'Marketing';
